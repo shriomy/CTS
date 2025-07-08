e241ee50c03dcaff6667c51ea552de0b0b8bc394
shriomy <snimashafernandopulle@gmail.com>
2025-07-08 21:03:20 +0530

    first commit: created spring boot app

--- backend/src/main/java/com/example/cts/cts/controller/testController.java
+++ backend/src/main/java/com/example/cts/cts/controller/testController.java
@@ -1,4 +1,15 @@
 package com.example.cts.cts.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/cts")
 public class testController {
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Test endpoint is working!";
+    }
 }
